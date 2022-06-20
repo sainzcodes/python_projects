bff6935bc23d4fe8acfc284f4c7a80287a4b16d1
sainzcodes <sai15001@byui.edu>
2022-06-19 19:24:07 -0600

    Update to seconds in an hour

--- Python/First Steps/Seconds in an hour.py
+++ Python/First Steps/Seconds in an hour.py
@@ -1,8 +1,11 @@
 ## Calculate second in an hour
 seconds_per_hour = 60 * 60
 print  (seconds_per_hour)
+#How many hours in a day
 day = 24
+#How many seconds in a day
 seconds_per_day = seconds_per_hour * day
+#Results
 print(seconds_per_day)
 print (seconds_per_day/seconds_per_hour)
 print (seconds_per_day//seconds_per_hour)
