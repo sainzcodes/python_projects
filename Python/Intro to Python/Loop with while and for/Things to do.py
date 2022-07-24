04b629d59c597ba9cdba5ca7ffa45d268040036b
sainzcodes <sai15001@byui.edu>
2022-07-23 22:56:04 -0600

    All

--- Python/Intro to Python/Loop with while and for/Things to do.py
+++ Python/Intro to Python/Loop with while and for/Things to do.py
@@ -1,8 +1,8 @@
 #Use a for loop to print the values of the list [3, 2, 1, 0]
-
-for value in [3, 2, 1, 0]
+'''
+for value in [3, 2, 1, 0]:
 	print(value)
-
+'''
 
 ''' 6.2 Assign the value 7 to the variable guess_me, and the value 1 to the variable number. 
 Write a while loop that compares number with guess_me. Print 'too low' if number is less than guess me. 
@@ -26,7 +26,7 @@ while True:
 ''' Assign the value 5 to the variable guess_me. Use a for loop to iterate a variable called number over range(10). 
 If number is less than guess_me, print 'too low'. If it equals guess_me, print found it! and then break out of the 
 for loop. If number is greater than guess_me, print 'oops' and then exit the loop. '''
-
+'''
 guess_me = 5
  for number in range(10):
      if number < guess_me:
@@ -37,3 +37,4 @@ guess_me = 5
      else:
          print("oops")
          break
+         '''
---
+++ Python/Intro to Python/Loop with while and for/lean.py
@@ -0,0 +1,18 @@
+guess_me = 7
+number = 23
+while True: 
+	if number > guess_me:
+		print("Too high")
+		break
+	elif number == guess_me:
+		print("found it!")
+		break
+	elif  number < guess_me:
+		print("Too low")
+		break
+number+=1
+
+
+guess_me = 5
+number = 1 
+while True: 
