b13f617f233f2f0ab59971540301b069bc6291dd
sainzcodes <sai15001@byui.edu>
2022-07-26 22:45:50 -0600

    Enter Tuples and Lists

---
+++ Python/Intro to Python/Tuples and Lists/syntax.py
@@ -0,0 +1,10 @@
+#The following are examples on the syntax
+empty_tuple = ()
+print(empty_tuple)
+
+#If you have more than one element, follow all but the last one with a comma:
+marx_tuple = 'Groucho', 'Chico', 'Harpo'
+
+
+print(marx_tuple)
+
