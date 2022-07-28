da2e9d86d80af594a5ec016cb0efbe0c5653cbb5
sainzcodes <sai15001@byui.edu>
2022-07-27 23:31:18 -0600

    072722

--- Python/Intro to Python/Tuples and Lists/syntax.py
+++ Python/Intro to Python/Tuples and Lists/syntax.py
@@ -1,10 +1,8 @@
 #The following are examples on the syntax
-empty_tuple = ()
-print(empty_tuple)
+#empty_tuple = ()
+#print(empty_tuple)
 
 #If you have more than one element, follow all but the last one with a comma:
 marx_tuple = 'Groucho', 'Chico', 'Harpo'
-
-
 print(marx_tuple)
 
