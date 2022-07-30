1c8133b16e9653772899c1cf04a5783f583458ef
sainzcodes <sai15001@byui.edu>
2022-07-29 22:44:03 -0600

    Watching John Wick

--- Python/Intro to Python/Tuples and Lists/syntax.py
+++ Python/Intro to Python/Tuples and Lists/syntax.py
@@ -23,8 +23,7 @@ icecream = 'tuttifrutti'
 password, icecream = icecream, password
 
 #Iterate with for and in
-
-Tuple iteration is like iteration of other types:
+#Tuple iteration is like iteration of other types:
 
 words = ('fresh','out', 'of', 'ideas')
 for word in words:
@@ -39,3 +38,21 @@ first_names = ['Graham', 'John', 'Terry', 'Terry', 'Michael']
 leap_years = [2000, 2004, 2008]
 randomness = ['Punxsatawney", {"groundhog": "Phil"}, "Feb. 2"}']
 
+
+# This example converts a tuple to a list:
+a_tuple = ('ready', 'fire', 'aim')
+list(a_tuple)
+
+#Get an Item by [ offset ]
+marxes = ['Groucho', 'Chico', 'Harpo']
+print(marxes[0:2])
+
+#Combine Lists by Using extend() or +
+marxes = ['Groucho', 'Chico', 'Harpo', 'Zeppo']
+others = ['Gummo', 'Karl']
+marxes += others
+
+#Change Items with a Slice
+#If you’re not sure or don’t care where the item is in the list, use remove() to delete it by value. Goodbye, Groucho:
+marxes = ['Groucho', 'Chico', 'Harpo']
+marxes.remove('Groucho')
